24456cd166f031940640ae14c0cf25ebdaf8bf51
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2023-02-09 12:41:18 -0600

    Finished GitHub API Wrapper

--- backend/github.py
+++ backend/github.py
@@ -4,40 +4,38 @@ import config
 
 class GitHubAPI:
     """GitHub API wrapper class"""
-    def __init__(self, username, repo_name):
-        self.username = username
-        self.repo_name = repo_name
+    def __init__(self, repo_url):
+        self.repo_url = repo_url
+        self.username = repo_url.split('/')[3]
+        self.repo_name = repo_url.split('/')[4]
         self.base_url = f"https://api.github.com/repos/{self.username}/{self.repo_name}"
+        self.search_url = f"https://api.github.com/search/issues?q=repo:{self.username}/{self.repo_name}"
         self.auth_headers = {'Authorization': 'token ' + config.github_token}
-        self.repo = requests.get(self.base_url, headers=self.auth_headers).json()
-
-    def get_repo_url(self):
-        """Get the repo URL"""
-        return self.repo["html_url"]
+        self.repo_data = requests.get(self.base_url, headers=self.auth_headers).json()
 
     def get_repo_description(self):
         """Get the repo description"""
-        return self.repo["description"]
+        return self.repo_data["description"]
 
     def get_stargazers_count(self):
         """Get the number of stargazers (people who starred the repo)"""
-        return self.repo["stargazers_count"]
+        return self.repo_data["stargazers_count"]
 
     def get_forks_count(self):
         """Get the number of forks (people who forked the repo)"""
-        return self.repo["forks_count"]
+        return self.repo_data["forks_count"]
 
     def get_watchers_count(self):
         """Get the number of watchers (people who watched the repo)"""
-        return self.repo["watchers_count"]
+        return self.repo_data["watchers_count"]
 
     def get_open_issues_count(self):
         """Get the number of open issues"""
-        return self.repo["open_issues_count"]
+        return self.repo_data["open_issues_count"]
 
     def get_topics(self):
         """Get the topics"""
-        return self.repo["topics"]
+        return self.repo_data["topics"]
     
     def get_issues(self):
         """Get the issues"""
@@ -49,13 +47,27 @@ class GitHubAPI:
                     for i in range(len(issue['labels'])):
                         if issue['labels'][i]['name'] not in issue_labels:
                             issue_labels.append(issue['labels'][i]['name'])
-            except: 
+            except Exception as e:
+                print(e)
                 pass
         return issue_labels
 
+    def get_date_of_last_commit(self):
+        """Get the date of the last commit"""
+        commits_json = requests.get(self.base_url + "/commits", headers=self.auth_headers).json()
+        date_of_last_commit = commits_json[0]["commit"]["author"]["date"].split("T")[0]
+        return date_of_last_commit
+
+    def get_date_of_last_merged_pull_request(self):
+        """Get the date of the last merged pull request"""
+        pulls_json = requests.get(self.search_url + "+is:pr+is:merged", headers=self.auth_headers).json()
+        date_of_last_merged_pull_request = pulls_json["items"][0]["closed_at"].split("T")[0]
+        return date_of_last_merged_pull_request
+
 if __name__ == '__main__':
-    gh = GitHubAPI('up-for-grabs', 'up-for-grabs.net') # GitHub API wrapper
-    gh.get_issues()
+    repo_url = 'https://github.com/up-for-grabs/up-for-grabs.net'
+    gh = GitHubAPI(repo_url)
+    gh.get_date_of_last_merged_pull_request()
     
 
     
--- backend/pop_db.py
+++ backend/pop_db.py
@@ -10,7 +10,6 @@ import mysql.connector
 import config
 from github import GitHubAPI
 
-
 class PopulateDB:
     """Populate the database with data from GitHub API"""
     def __init__(self):
@@ -21,12 +20,14 @@ class PopulateDB:
                         database=config.db_database
                     )
 
-    def pop_project(self, gh_username, repo_name):
+    def pop_project(self, gh_repo_url):
         """Populate the project table"""
-        gh = GitHubAPI(gh_username, repo_name) # GitHub API wrapper
+        gh = GitHubAPI(gh_repo_url) # GitHub API wrapper
+
+        gh_repo_name = gh.repo_name
+        gh_username = gh.username
 
         # Get the data from GitHub API
-        gh_repo_url = gh.get_repo_url()
         gh_description = gh.get_repo_description()
 
         # Topics / tech stack
@@ -37,9 +38,16 @@ class PopulateDB:
         gh_issues = [''] * 3 # empty list of 3 strings to store the issues
         gh_issues[:len(gh.get_issues())] = gh.get_issues() # get the issues
 
+        # Stars, forks, watchers count
         gh_stargazers_count = gh.get_stargazers_count()
         gh_forks_count = gh.get_forks_count()
         gh_watchers_count = gh.get_watchers_count()
+
+        # Date of last commit
+        gh_date_of_last_commit = gh.get_date_of_last_commit()
+
+        # Data of last MERGED pull request
+        gh_date_of_last_merged_pull_request = gh.get_date_of_last_merged_pull_request()
 
         # Insert the data into the database
         try:
@@ -47,11 +55,11 @@ class PopulateDB:
 
             topics_cols = "gh_topics0, gh_topics1, gh_topics2, gh_topics3, gh_topics4, gh_topics5" # Topic column names
             issues_cols = "issue_label_1, issue_label_2, issue_label_3" # Issues column names
-            cols = f"(gh_repo_name, gh_repo_url, gh_description, gh_username, num_stars, num_forks, num_watchers, {topics_cols}, {issues_cols})" # All column names
+            cols = f"(gh_repo_name, gh_repo_url, gh_description, gh_username, num_stars, num_forks, num_watchers, {topics_cols}, {issues_cols}, date_last_commit, date_last_merged_PR)" # All column names
 
             topics_vals = f"N'{gh_topics[0]}', N'{gh_topics[1]}', N'{gh_topics[2]}', N'{gh_topics[3]}', N'{gh_topics[4]}', N'{gh_topics[5]}'" # Topic values
             issues_vals = f"N'{gh_issues[0]}', N'{gh_issues[1]}', N'{gh_issues[2]}'" # Issues values
-            values = f"(N'{repo_name}', N'{gh_repo_url}', N'{gh_description}', N'{gh_username}', {gh_stargazers_count}, {gh_forks_count}, {gh_watchers_count}, {topics_vals}, {issues_vals})" # All values
+            values = f"(N'{gh_repo_name}', N'{gh_repo_url}', N'{gh_description}', N'{gh_username}', {gh_stargazers_count}, {gh_forks_count}, {gh_watchers_count}, {topics_vals}, {issues_vals}, N'{gh_date_of_last_commit}', N'{gh_date_of_last_merged_pull_request}')" # All values
             
             q = f"INSERT INTO projects {cols} VALUES {values}"
             cursor.execute(q)
@@ -65,4 +73,4 @@ class PopulateDB:
 
 if __name__ == '__main__':
     pop = PopulateDB()
-    pop.pop_project('up-for-grabs', 'up-for-grabs.net')
+    pop.pop_project('https://github.com/up-for-grabs/up-for-grabs.net')
