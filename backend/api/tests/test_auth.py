1a10500c70341d39fd1181754271d7637c4d75f0
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2023-07-31 13:36:56 -0500

    updates unittests

--- backend/api/tests/test_auth.py
+++ backend/api/tests/test_auth.py
@@ -1,12 +1,11 @@
 import os
 import sys
 import unittest
+import requests
 from flask import Flask
 from dotenv import load_dotenv
-import requests
 
-# set path to backend\api
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # set path to backend\api
 from app import create_app
 
 class AuthTestCase(unittest.TestCase):
@@ -70,19 +69,19 @@ class AuthTestCase(unittest.TestCase):
     def test_github_login(self):
         load_dotenv()
             
-        # Replace these values with your own
+        # grab the client ID and secret from the environment
         client_id = os.environ.get('GITHUB_CLIENT_ID')
         client_secret = os.environ.get('GITHUB_CLIENT_SECRET')
         redirect_uri = 'http://127.0.0.1:3000/registration'
 
-        # # Step 1: Redirect the user to the authorization URL
+        # redirect to the authorization URL
         auth_url = f'https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=user'
         print(f'URL to authorize the app: {auth_url}')
 
-        # # Step 2: Obtain a temporary code from the redirect URL
+        # obtain the temporary code from the redirect URL
         code = input('Enter code from the redirect URL: ')
 
-        # # Step 3: Exchange the temporary code for an access token
+        # exchange temporary code for access token
         token_url = 'https://github.com/login/oauth/access_token'
         token_params = {
             'client_id': client_id,
@@ -95,21 +94,20 @@ class AuthTestCase(unittest.TestCase):
         token_response = requests.post(token_url, params=token_params, headers=token_headers)
         access_token = token_response.json().get('access_token')
 
-        # # Step 4: Use the access token to fetch the user's GitHub profile information
+        # fetch the user's profile the with access token
         profile_url = 'https://api.github.com/user'
         profile_headers = {'Authorization': f'Bearer {access_token}', 'Accept': 'application/json'}
         profile_response = requests.get(profile_url, headers=profile_headers)
         username = profile_response.json().get('login')
         self.assertEqual(username, 'peter-w-bryant')
 
-        # # Step 5: Send a GET request to the /github_login endpoint with the code parameter
+        # test login with GitHub
         app = create_app()
         with app.test_client() as client:
             login_params = {'code': code}
             response = client.get('/logout', json=login_params)
             self.assertEqual(response.status_code, 200)
             self.assertIn('access_token', response.json)
-
 
 if __name__ == '__main__':
     unittest.main()
