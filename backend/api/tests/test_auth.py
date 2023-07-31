5a77fca83dcacde0fe3a27e4f146747bb8378488
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2023-07-31 17:05:31 -0500

    updates to github login unittest

--- backend/api/tests/test_auth.py
+++ backend/api/tests/test_auth.py
@@ -67,7 +67,12 @@ class AuthTestCase(unittest.TestCase):
             self.assertEqual(response.status_code, 401) # 401 Unauthorized
         
     def test_github_login(self):
-        # test login with GitHub using personal access token (hidden)
+        # Test login with GitHub using personal access token (hidden)
+        
+        # Current limitations: can't pass real code without using web UI
+        # Separate test below (test_github_login2) lets you generate code in browser and pass it to the API, getting "bad_verification_code"
+        # see https://docs.github.com/en/apps/oauth-apps/maintaining-oauth-apps/troubleshooting-oauth-app-access-token-request-errors
+
         load_dotenv()
         app = create_app()
         with app.test_client() as client:
@@ -75,6 +80,50 @@ class AuthTestCase(unittest.TestCase):
             response = client.get('/github_login', json=login_params, query_string={'testing_access_token': True})
             self.assertEqual(response.status_code, 200)
             self.assertIn('access_token', response.json)
+
+    # def test_github_login2(self):
+    #     load_dotenv()
+            
+    #     # grab the client ID and secret from the environment
+    #     client_id = os.environ.get('GITHUB_CLIENT_ID')
+    #     client_secret = os.environ.get('GITHUB_CLIENT_SECRET')
+    #     redirect_uri = 'http://127.0.0.1:3000/registration'
+
+    #     # redirect to the authorization URL
+    #     auth_url = f'https://github.com/login/oauth/authorize?client_id={client_id}&redirect_uri={redirect_uri}&scope=user'
+    #     print(f'URL to authorize the app: {auth_url}')
+
+    #     # obtain the temporary code from the redirect URL
+    #     code = input('Enter code from the redirect URL: ')
+
+    #     # exchange temporary code for access token
+    #     token_url = 'https://github.com/login/oauth/access_token'
+    #     token_params = {
+    #         'client_id': client_id,
+    #         'client_secret': client_secret,
+    #         'code': code,
+    #         'redirect_uri': redirect_uri
+    #     }
+
+    #     token_headers = {'Accept': 'application/json'}
+    #     token_response = requests.post(token_url, params=token_params, headers=token_headers)
+    #     access_token = token_response.json().get('access_token')
+    #     access_token = os.environ.get('GITHUB_TOKEN')
+
+    #     # fetch the user's profile the with access token
+    #     profile_url = 'https://api.github.com/user'
+    #     profile_headers = {'Authorization': f'Bearer {access_token}', 'Accept': 'application/json'}
+    #     profile_response = requests.get(profile_url, headers=profile_headers)
+    #     username = profile_response.json().get('login')
+    #     self.assertEqual(username, 'peter-w-bryant')
+
+    #     # test login with GitHub
+    #     app = create_app()
+    #     with app.test_client() as client:
+    #         login_params = {'code': code}
+    #         response = client.get('/github_login', json=login_params)
+    #         self.assertEqual(response.status_code, 200)
+    #         self.assertIn('access_token', response.json)
 
 if __name__ == '__main__':
     unittest.main()
