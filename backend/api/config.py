cbcfdffb5f62fdeb5b8a99a1488ddd7a5d7096bf
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2023-08-13 21:01:30 -0500

    updated config

--- backend/api/config.py
+++ backend/api/config.py
@@ -3,31 +3,31 @@ from dotenv import load_dotenv
 
 class Config:
     load_dotenv()
-    TESTING = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db" # database file path
-    PROJECT_BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # project base directory
-    OAUTHLIB_INSECURE_TRANSPORT = 1 # allow insecure transport for testing
-
-    # Auth Tokens
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID')
     GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET')
     GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+
+    PROJECT_BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    OAUTHLIB_INSECURE_TRANSPORT = 1 
 
 class DevelopmentConfig(Config):
     ENV = 'development'
     DEBUG = True
     FLASK_DEBUG = 1
     DEVELOPMENT = True
-    JWT_SECRET_KEY = "secret_for_test_environment"
-    SECRET_KEY = "secret_for_test_environment"
+    TESTING = True
 
 class ProductionConfig(Config):
     ENV = 'production'
     DEBUG = False
+    FLASK_DEBUG = 0
     DEVELOPMENT = False
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    TESTING = False
 
-# Dictionary of config classes
 config = {
     'development': DevelopmentConfig,
     'production': ProductionConfig
