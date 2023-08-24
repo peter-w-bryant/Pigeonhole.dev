0b54dbe2d8cfab13a10ba74f330ecd178f884a0f
bryxli <bryanzero765@gmail.com>
2023-08-24 16:49:09 -0500

    redux store

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { createStore, applyMiddleware, Store } from "redux"
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
+
 import App from './App.tsx'
+import reducer from "./store/reducer"
+
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const store: Store<ProjectState, ProjectAction> & {
+  dispatch: DispatchType
+} = createStore(reducer, applyMiddleware(thunk))
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
