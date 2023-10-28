7ff3438a4b758ea1764dc6d22457c118699ffc5f
Peter Bryant <72423203+peter-w-bryant@users.noreply.github.com>
2023-10-28 11:34:12 -0500

    updated index route

--- pigeonhole/routes/frontend/index.py
+++ pigeonhole/routes/frontend/index.py
@@ -18,15 +18,14 @@ index = Blueprint('index', __name__) # blueprint for auth routes
 @index.route('/', methods=['GET', 'POST'])
 def get_index():
     
-    if request.method == 'GET':
-        
-        # Request all projects from the database
-        all_projects_response = requests.get('http://localhost:5000/api/projects/all-projects')
-        if all_projects_response.status_code == 200:
-            all_projects_json = all_projects_response.json()
-            
-            return render_template('index.html', projects=all_projects_json)
-        
-        return render_template('index.html')
+    # Build all projects payload
+    all_projects_response = requests.get('http://localhost:5000/api/projects/all-projects')
+    if all_projects_response.status_code == 200:
+        all_projects_json = all_projects_response.json()
+    else:
+        all_projects_json = {}
+    
+    if request.method == 'POST':
+        return render_template('index.html', projects=all_projects_json)
 
-    return render_template('index.html')
+    return render_template('index.html', projects=all_projects_json)
